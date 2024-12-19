876a2ba0696e38c0f5fe62c2d4b180f20cb695ce
Matin <mkomijani323@yahoo.com>
2024-12-19 14:36:09 +0330

    making success page

--- script.js
+++ script.js
@@ -1,33 +1,60 @@
 var emailInput = document.querySelector('.email');
-var submitBtn = document.querySelector('#text form button');
+var submitBtn = document.querySelector('.button');
 var formLabel = document.querySelector('.formLabel');
-var clicked = false;
-var hasError = false;
-var errorMassage = document.createElement('p');
-submitBtn.addEventListener('click', function (e) {
-    e.preventDefault();
-    emailInput.style.borderColor = 'hsl(0, 0%, 80%)';
-    emailInput.style.color = 'black';
-    // formLabel.removeChild(errorMassage);
-    errorMassage.remove();
-    if (emailInput.value === '' || !emailInput.checkValidity()) {
-        emailInput.style.borderColor = 'hsl(4, 100%, 67%)';
-        emailInput.style.color = 'hsl(4, 100%, 67%)';
-        clicked = true;
-        emailInput.classList.add('email2');
-        if (!hasError) {
-            hasError = true;
-            errorMassage.innerHTML = 'Valid email required';
-            errorMassage.classList.add('emailError');
-            formLabel.append(errorMassage);
+var text = document.getElementById('text');
+var design = document.getElementById('design');
+var container = document.querySelector('body div');
+//checking validations from DOM
+if (emailInput && submitBtn && formLabel && text && design && container) {
+    var clicked = false;
+    var hasError = false;
+    var errorMassage_1 = document.createElement('p');
+    submitBtn.addEventListener('click', function (e) {
+        e.preventDefault();
+        emailInput.style.borderColor = 'hsl(0, 0%, 80%)';
+        emailInput.style.color = 'black';
+        errorMassage_1.remove();
+        if (emailInput.value === '' || !emailInput.checkValidity()) {
+            emailInput.style.borderColor = 'hsl(4, 100%, 67%)';
+            emailInput.style.color = 'hsl(4, 100%, 67%)';
+            clicked = true;
+            emailInput.classList.add('email2');
+            if (!hasError) {
+                hasError = true;
+                errorMassage_1.innerHTML = 'Valid email required';
+                errorMassage_1.classList.add('emailError');
+                formLabel.append(errorMassage_1);
+            }
         }
-    }
-    else {
-        clicked = false;
-    }
-});
-emailInput.addEventListener('focus', function () {
-    if (clicked) {
-        emailInput.style.borderColor = 'hsl(4, 100%, 67%)';
-    }
-});
+        else {
+            //saving in user e-mail
+            var userEmail = emailInput.value;
+            // make a blank container
+            clicked = false;
+            text.remove();
+            design.remove();
+            container.classList.add('successMassage');
+            // new elements for success massage
+            //check image
+            var checkImg = document.createElement('img');
+            checkImg.src = './assets/images/icon-success.svg';
+            // Label of success massage
+            var successLabel = document.createElement('h1');
+            successLabel.innerHTML = 'Thanks for subscribing!';
+            //description of success massage
+            var description = document.createElement('p');
+            description.innerHTML = 'A confirmation email has been sent to <p class="userEmail">' + userEmail + '</p>. Please open it and click the button inside to confirm your subscription.';
+            //Dismiss button
+            var dismissBtn = document.createElement('button');
+            dismissBtn.innerHTML = 'Dismiss message';
+            dismissBtn.classList.add('button');
+            //append elements to success massage div
+            container.append(checkImg, successLabel, description, dismissBtn);
+        }
+    });
+    emailInput.addEventListener('focus', function () {
+        if (clicked) {
+            emailInput.style.borderColor = 'hsl(4, 100%, 67%)';
+        }
+    });
+}
--- script.ts
+++ script.ts
@@ -1,37 +1,73 @@
-const emailInput = document.querySelector('.email') as HTMLInputElement;
-const submitBtn = document.querySelector('#text form button') as HTMLButtonElement;
-const formLabel = document.querySelector('.formLabel');
+const emailInput = document.querySelector('.email') as HTMLInputElement | null;
+const submitBtn = document.querySelector('.button') as HTMLButtonElement | null;
+const formLabel = document.querySelector('.formLabel') as HTMLDivElement | null;
+const text = document.getElementById('text') as HTMLDivElement | null;
+const design = document.getElementById('design') as HTMLDivElement | null;
+const container = document.querySelector('body div') as HTMLDivElement | null;
 
-var clicked: boolean = false;
-var hasError: boolean = false;
+//checking validations from DOM
+if (emailInput && submitBtn && formLabel && text && design && container) {
+    var clicked: boolean = false;
+    var hasError: boolean = false;
 
-let errorMassage = document.createElement('p');
+    let errorMassage = document.createElement('p');
 
-submitBtn.addEventListener('click', (e) => {
-    e.preventDefault();
-    emailInput.style.borderColor = 'hsl(0, 0%, 80%)';
-    emailInput.style.color = 'black';
-    // formLabel.removeChild(errorMassage);
-    errorMassage.remove();
-    if (emailInput.value === '' || !emailInput.checkValidity()) {
-        emailInput.style.borderColor = 'hsl(4, 100%, 67%)';
-        emailInput.style.color = 'hsl(4, 100%, 67%)';
-        clicked = true;
-        emailInput.classList.add('email2');
-        if (!hasError) {
-            hasError = true;
-            errorMassage.innerHTML = 'Valid email required';
-            errorMassage.classList.add('emailError');
-            formLabel.append(errorMassage);
+    submitBtn.addEventListener('click', (e) => {
+        e.preventDefault();
+        emailInput.style.borderColor = 'hsl(0, 0%, 80%)';
+        emailInput.style.color = 'black';
+        errorMassage.remove();
+        if (emailInput.value === '' || !emailInput.checkValidity()) {
+            emailInput.style.borderColor = 'hsl(4, 100%, 67%)';
+            emailInput.style.color = 'hsl(4, 100%, 67%)';
+            clicked = true;
+            emailInput.classList.add('email2');
+            if (!hasError) {
+                hasError = true;
+                errorMassage.innerHTML = 'Valid email required';
+                errorMassage.classList.add('emailError');
+                formLabel.append(errorMassage);
+            }
+        } else {
+            //saving in user e-mail
+            var userEmail: string = emailInput.value;
+
+            // make a blank container
+            clicked = false;
+            text.remove();
+            design.remove();
+            container.classList.add('successMassage');
+            // new elements for success massage
+
+            //check image
+            let checkImg = document.createElement('img') as HTMLImageElement;
+            checkImg.src = './assets/images/icon-success.svg';
+
+            // Label of success massage
+            let successLabel = document.createElement('h1');
+            successLabel.innerHTML = 'Thanks for subscribing!';
+
+            //description of success massage
+            let description = document.createElement('p');
+            description.innerHTML = 'A confirmation email has been sent to <p class="userEmail">' + userEmail + '</p>. Please open it and click the button inside to confirm your subscription.';
+
+
+            //Dismiss button
+            let dismissBtn = document.createElement('button');
+            dismissBtn.innerHTML = 'Dismiss message';
+            dismissBtn.classList.add('button');
+
+
+            //append elements to success massage div
+            container.append(checkImg, successLabel, description, dismissBtn);
         }
-    }else {
-        clicked=false;
-    }
 
-});
 
-emailInput.addEventListener('focus', () => {
-    if (clicked) {
-        emailInput.style.borderColor = 'hsl(4, 100%, 67%)';
-    }
-});
+    });
+
+    emailInput.addEventListener('focus', () => {
+        if (clicked) {
+            emailInput.style.borderColor = 'hsl(4, 100%, 67%)';
+        }
+    });
+}
